92bc3b77159c628e7ddec39de50236fd019c6224
RondeXx <canumut0066@hotmail.com>
2021-11-15 15:42:55 +0300

    Admin panel login++
    crud compleate

--- MovieApp/MovieApp.UI/Controllers/AdminController.cs
+++ MovieApp/MovieApp.UI/Controllers/AdminController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using MovieApp.Service.Models;
+using MovieApp.UI.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,11 +14,12 @@ namespace MovieApp.UI.Controllers
 {
     public class AdminController : Controller
     {
-
+        private readonly IWebHostEnvironment _webHostEnvironment;
         private readonly MovieAppContext _context; //Context imiz tekrardan yenilensin her çalıştıgında
-        public AdminController(MovieAppContext context) // startup yazdık
+        public AdminController(MovieAppContext context, IWebHostEnvironment webHostEnvironment) // startup yazdık
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -22,32 +27,56 @@ namespace MovieApp.UI.Controllers
             return View();
         }
 
-        
+
 
         public IActionResult List()
         {
-        var result =_context.Movies.ToList();
+            var result = _context.Movies.ToList();
             return View(result);
         }
 
         public IActionResult Delete(int Id)
-            {
-            _context.Movies.Remove(_context.Movies.Find(Id));
+        {
+            var result = _context.Movies.Find(Id);
+            _context.Movies.Remove(result);
+
+            string url = _webHostEnvironment.WebRootPath + result.MovieImage;
+            //dosyanın tüm yolunu aldık hatasız silmek için
+            FileHelper.DeleteFile(url);
+
+
             _context.SaveChanges();
             return RedirectToAction("List");
         }
 
         public IActionResult Edit(int Id)
         {
+
             var result = _context.Movies.Find(Id);
+
             return View(result);
         }
         [HttpPost]
-        public IActionResult Edit(Movy movie)
+        public IActionResult Edit(Movy movie, IFormFile image, IFormFile video)
         {
-            _context.Entry(movie).State = EntityState.Modified; //Entities modified update
-            _context.SaveChanges();
-            return RedirectToAction("List");
+            //Entities modified update
+            if (image == null && video == null)
+            {
+                return RedirectToAction("Edit");
+            }
+            else
+            {
+                string url = _webHostEnvironment.WebRootPath + movie.MovieImage; //tüm dosyanın a dan z ye tüm yolunu bul ve image yolunu ekle
+                FileHelper.DeleteFile(url); // tüm bulunan yolu url paramtresi gönder delet helperine sonra sil
+                url = _webHostEnvironment.WebRootPath + movie.MovieVideo;
+                FileHelper.DeleteFile(url);
+
+                movie.MovieImage = FileHelper.UploadFile(image, "images"); //iki tane parametre isityor biri file digeri yolu
+                movie.MovieVideo = FileHelper.UploadFile(video, "videos");
+                _context.Entry(movie).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("List");
+            }
         }
 
         public IActionResult Update(int Id)
@@ -55,19 +84,32 @@ namespace MovieApp.UI.Controllers
             var result = _context.Movies.Find(Id);
             return View(result);
         }
-        
+
         public IActionResult Add()
         {
-                
             return View();
         }
+
         [HttpPost]
-        public IActionResult Add(Movy model)
+        public IActionResult Add(Movy model, IFormFile image, IFormFile video)
         {
-            _context.Movies.Add(model);
-            _context.SaveChanges();
-            return RedirectToAction("List");
+            if (image == null && video == null)
+            {
+                return RedirectToAction("Add");
+            }
+            else
+            {
+                model.MovieImage = FileHelper.UploadFile(image, "images");
+                model.MovieVideo = FileHelper.UploadFile(video, "videos");
+                _context.Movies.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("List");
+            }
+
+
+
         }
+
     }
 }
 
---
+++ MovieApp/MovieApp.UI/Helpers/FileHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace MovieApp.UI.Helpers
+{
+    public class FileHelper
+    {
+        public static string UploadFile(IFormFile file, string path)
+        {
+         
+
+            string SavePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/" + path, file.FileName);
+            using (var stream = new FileStream(SavePath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+            return "/" + path + "/" + file.FileName;
+        }
+
+        public static bool DeleteFile(string path)
+        {
+            if (File.Exists(path) == true)
+            //Exists true yada false dönen bir metod true ise yani dosya var ise işlem yap dedik
+            {
+                File.Delete(path); //true imiş delete ile gelen url komple sil
+                return true;
+            }
+            return false;
+        }
+    }
+}
